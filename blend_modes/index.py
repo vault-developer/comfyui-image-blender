fb4bb237b10bded0e83e6b253d09d3942d2414f4
Albert Trott <trott.alik@gmail.com>
2024-07-30 22:56:05 +0200

    feat: add binary functions

--- blend_modes/index.py
+++ blend_modes/index.py
@@ -1,7 +1,9 @@
 from .normal import normal_blend_functions
 from .arithmetic import arithmetic_blend_functions
+from .binary import binary_blend_functions
 
 blend_functions = {}
 
 blend_functions.update(normal_blend_functions)
 blend_functions.update(arithmetic_blend_functions)
+blend_functions.update(binary_blend_functions)
--- helpers.py
+++ helpers.py
@@ -2,3 +2,9 @@ import torch
 
 def replace_zeros(tensor: torch.Tensor, epsilon: float = 1e-7) -> torch.Tensor:
     return torch.where(tensor == 0, torch.tensor(epsilon, device=tensor.device), tensor)
+
+def float_to_uint8(tensor: torch.Tensor) -> torch.Tensor:
+    return (tensor * 256).to(torch.uint8)
+
+def uint8_to_float(tensor: torch.Tensor) -> torch.Tensor:
+    return torch.clamp(tensor/256, 0.0, 1.0)
