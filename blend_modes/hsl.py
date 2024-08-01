42887889a2b1e365ac9df09e66210fe3827dea76
Albert Trott <trott.alik@gmail.com>
2024-08-01 23:50:58 +0200

    feat: add hsl_color

---
+++ blend_modes/hsl.py
@@ -0,0 +1,85 @@
+from ..helpers import get_lightness, set_lightness
+from ..blend_modes_enum import BlendModes
+import torch
+
+# def hsi_intensity(base_image: torch.Tensor, blend_image: torch.Tensor) -> torch.Tensor:
+#     assert base_image.shape == blend_image.shape, "Base and blend images must have the same shape"
+#     assert base_image.shape[-1] == 3, "Input images must have 3 channels (RGB)"
+#
+#     blend_image_lightness = get_intensity(blend_image)
+#     result = set_intensity(base_image, blend_image_lightness)
+#     return result.clamp(0, 1)
+#
+# def hsi_increase_intensity(base_image: torch.Tensor, blend_image: torch.Tensor) -> torch.Tensor:
+#     assert base_image.shape == blend_image.shape, "Base and blend images must have the same shape"
+#     assert base_image.shape[-1] == 3, "Input images must have 3 channels (RGB)"
+#
+#     blend_image_lightness = get_intensity(blend_image)
+#     result = add_intensity(base_image, blend_image_lightness)
+#     return result.clamp(0, 1)
+#
+# def hsi_decrease_intensity(base_image: torch.Tensor, blend_image: torch.Tensor) -> torch.Tensor:
+#     assert base_image.shape == blend_image.shape, "Base and blend images must have the same shape"
+#     assert base_image.shape[-1] == 3, "Input images must have 3 channels (RGB)"
+#
+#     blend_image_lightness = get_intensity(blend_image) - 1
+#     result = add_intensity(base_image, blend_image_lightness)
+#     return result.clamp(0, 1)
+#
+# def hsi_saturation(base_image: torch.Tensor, blend_image: torch.Tensor) -> torch.Tensor:
+#     intensity = get_intensity(base_image)
+#     saturation = get_saturation_hsi(blend_image)
+#     result = set_saturation_hsi(base_image, saturation)
+#     result = set_intensity(result, intensity)
+#     return result.clamp(0, 1)
+#
+# def hsi_increase_saturation(base_image: torch.Tensor, blend_image: torch.Tensor) -> torch.Tensor:
+#     base_image_saturation = get_saturation_hsi(base_image)
+#     blend_image_saturation = get_saturation_hsi(blend_image)
+#
+#     new_saturation = torch.lerp(base_image_saturation, torch.ones_like(base_image_saturation), blend_image_saturation)
+#
+#     base_image_lightness = get_intensity(base_image)
+#
+#     result = set_saturation_hsi(base_image, new_saturation)
+#     result = set_intensity(result, base_image_lightness)
+#
+#     return result.clamp(0, 1)
+#
+# def hsi_decrease_saturation(base_image: torch.Tensor, blend_image: torch.Tensor) -> torch.Tensor:
+#     base_image_saturation = get_saturation_hsi(base_image)
+#     blend_image_saturation = get_saturation_hsi(blend_image)
+#
+#     new_saturation = torch.lerp(torch.zeros_like(base_image_saturation), base_image_saturation, blend_image_saturation)
+#
+#     base_image_lightness = get_intensity(base_image)
+#
+#     result = set_saturation_hsi(base_image, new_saturation)
+#     result = set_intensity(result, base_image_lightness)
+#
+#     return result.clamp(0, 1)
+
+def hsl_color(base_image: torch.Tensor, blend_image: torch.Tensor) -> torch.Tensor:
+    base_image_intensity = get_lightness(base_image)
+    result = set_lightness(blend_image, base_image_intensity)
+    return result.clamp(0, 1)
+
+# def hsi_hue(base_image: torch.Tensor, blend_image: torch.Tensor) -> torch.Tensor:
+#     base_image_saturation = get_saturation_hsi(base_image)
+#     base_image_lightness = get_intensity(base_image)
+#
+#     result = blend_image.clone()
+#     result = set_saturation_hsi(result, base_image_saturation)
+#     result = set_intensity(result, base_image_lightness)
+#     return result.clamp(0, 1)
+
+hsl_blend_functions = {
+    BlendModes.HSL_COLOR: hsl_color,
+    # BlendModes.HSL_HUE: hsi_hue,
+    # BlendModes.HSL_SATURATION: hsi_saturation,
+    # BlendModes.HSL_LIGHTNESS: hsi_intensity,
+    # BlendModes.HSL_DECREASE_SATURATION: hsi_decrease_saturation,
+    # BlendModes.HSL_INCREASE_SATURATION: hsi_increase_saturation,
+    # BlendModes.HSL_DECREASE_LIGHTNESS: hsi_decrease_intensity,
+    # BlendModes.HSL_INCREASE_LIGHTNESS: hsi_increase_intensity,
+}
--- helpers.py
+++ helpers.py
@@ -9,6 +9,36 @@ def float_to_uint8(tensor: torch.Tensor) -> torch.Tensor:
 def uint8_to_float(tensor: torch.Tensor) -> torch.Tensor:
     return torch.clamp(tensor/256, 0.0, 1.0)
 
+def get_lightness(image: torch.Tensor) -> torch.Tensor:
+    max_vals, _ = torch.max(image, dim=-1)
+    min_vals, _ = torch.min(image, dim=-1)
+    lightness = (max_vals + min_vals) * 0.5
+    return lightness
+
+def set_lightness(image: torch.Tensor, new_lightness: torch.Tensor) -> torch.Tensor:
+    result = add_lightness(image, new_lightness - get_lightness(image))
+    return result.clamp(0, 1)
+
+def add_lightness(image: torch.Tensor, new_intensity: torch.Tensor) -> torch.Tensor:
+    image = image + new_intensity.unsqueeze(-1)
+
+    intensity = get_lightness(image)
+    min = torch.min(image, dim=-1).values
+    max = torch.max(image, dim=-1).values
+
+    # adjust overflows
+    mask_min = min < 0.0
+    iln = torch.where(mask_min, 1.0 / (intensity - min + 1e-8), torch.zeros_like(intensity))
+    image = torch.where(mask_min.unsqueeze(-1), intensity.unsqueeze(-1) + ((image - intensity.unsqueeze(-1)) * intensity.unsqueeze(-1)) * iln.unsqueeze(-1), image)
+
+    mask_max = (max > 1.0) & ((max - intensity) > torch.finfo(max.dtype).eps)
+    il = torch.where(mask_max, 1.0 - intensity, torch.zeros_like(intensity))
+    ixl = torch.where(mask_max, 1.0 / (max - intensity + 1e-8), torch.zeros_like(intensity))
+    image = torch.where(mask_max.unsqueeze(-1), intensity.unsqueeze(-1) + ((image - intensity.unsqueeze(-1)) * il.unsqueeze(-1)) * ixl.unsqueeze(-1), image)
+
+    return image.clamp(0, 1)
+
+
 def get_intensity(image: torch.Tensor) -> torch.Tensor:
     return torch.mean(image, dim=-1)
 
