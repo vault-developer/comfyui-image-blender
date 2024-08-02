f81ff79eefc611b02057aa317319e8aa00db2fb1
Albert Trott <trott.alik@gmail.com>
2024-08-02 20:54:20 +0200

    add flat_light

--- blend_modes/lighten.py
+++ blend_modes/lighten.py
@@ -1,3 +1,4 @@
+from .lighten_helpers import penumbra
 from ..blend_modes_enum import BlendModes
 import torch
 
@@ -55,6 +56,26 @@ def lighten_vivid_light(base_image: torch.Tensor, blend_image: torch.Tensor) -> 
 
     return result
 
+def lighten_flat_light(base_image: torch.Tensor, blend_image: torch.Tensor) -> torch.Tensor:
+    mask_src_zero = torch.isclose(blend_image, torch.tensor(0.0))
+    result = torch.zeros_like(base_image)
+
+    mask_src_nonzero = ~mask_src_zero
+    inv_src = 1 - blend_image[mask_src_nonzero]
+
+    sum_tensor = inv_src + base_image[mask_src_nonzero]
+    hard_mix_result = torch.where(sum_tensor > 1.0, torch.ones_like(sum_tensor), torch.zeros_like(sum_tensor))
+    penumbra_a_result = penumbra(base_image[mask_src_nonzero], blend_image[mask_src_nonzero], lighten_color_dodge)
+    penumbra_b_result = penumbra(blend_image[mask_src_nonzero], base_image[mask_src_nonzero], lighten_color_dodge)
+
+    result[mask_src_nonzero] = torch.where(
+        torch.isclose(hard_mix_result, torch.tensor(1.0)),
+        penumbra_b_result,
+        penumbra_a_result
+    )
+
+    return result
+
 lighten_blend_functions = {
     BlendModes.LIGHTEN_COLOR_DODGE: lighten_color_dodge,
     BlendModes.LIGHTEN_LINEAR_DODGE: lighten_linear_dodge,
@@ -63,7 +84,7 @@ lighten_blend_functions = {
     BlendModes.LIGHTEN_SCREEN: lighten_screen,
     BlendModes.LIGHTEN_PIN_LIGHT: lighten_pin_light,
     BlendModes.LIGHTEN_VIVID_LIGHT: lighten_vivid_light,
-    # BlendModes.LIGHTEN_FLAT_LIGHT: lighten_color_dodge,
+    BlendModes.LIGHTEN_FLAT_LIGHT: lighten_flat_light,
     # BlendModes.LIGHTEN_HARD_LIGHT: lighten_color_dodge,
     # BlendModes.LIGHTEN_SOFT_LIGHT_IFS_ILLUSIONS: lighten_color_dodge,
     # BlendModes.LIGHTEN_SOFT_LIGHT_PEGTOP_DELPHI: lighten_color_dodge,
